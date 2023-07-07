fade1b1336027ca98cd974e21bf9daaa54b113cb
rhh4x0r <rhh4x0r@qhsecure.com>
2023-07-07 10:40:57 -0600

    added env variables to tests

--- tests/logsnag_test.rs
+++ tests/logsnag_test.rs
@@ -2,23 +2,34 @@
 mod tests {
     use super::*;
 
+    use dotenv::dotenv;
+    use std::env;
+
     use logsnag::Logsnag;
-    use logsnag::models::InsightValue;
+    use logsnag::models::{ InsightValue, TagHashMap };
 
     #[tokio::test]
     async fn test_publish() {
-        //TODO: find a way to use ENV variables in tests easily
+
+        dotenv().ok();
+
         let logsnag = Logsnag::new(
-            "".to_string(),
-            "".to_string()
+            env::var("LOGSNAG_API_KEY").expect("No Logsnag API Key (LOGSNAG_API_KEY) found in environment variables."),
+            env::var("LOGSNAG_PROJECT").expect("No Logsnag Project (LOGSNAG_PROJECT) found in environment variables.")
         );
+
+        let mut tags = TagHashMap::new();
+
+        tags.insert("guild-id", "test-guild-id");
+        tags.insert("User_Name", "test-username-id");
 
         let publish_result = logsnag.publish(
             "test".to_string(),
             "Test Event".to_string(),
             Some("A description here".to_string()),
             Some("❤️".to_string()),
-            Some(true)
+            Some(true),
+            Some(tags),
         ).await;
 
         println!("{:?}", publish_result);
